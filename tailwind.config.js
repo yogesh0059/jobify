4d81000fec04e1626e45de01d28f60b5cf07a897
yogesh <yogeshgedar10@gmail.com>
2025-06-26 00:06:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // 👈 important for manual toggling
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
